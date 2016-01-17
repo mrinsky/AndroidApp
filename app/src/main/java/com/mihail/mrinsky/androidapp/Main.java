2f187838e5f8f3dfa5a0409da91499a0f21214a7
Valery Mrinsky <marvinner@gmail.com>
2016-01-17 14:16:25 +0400

    add newBrunch "FindMe"

--- app/src/main/java/com/mihail/mrinsky/androidapp/Main.java
+++ app/src/main/java/com/mihail/mrinsky/androidapp/Main.java
@@ -2,12 +2,25 @@ package com.mihail.mrinsky.androidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class Main extends AppCompatActivity {
+    Button screenColor;
+    RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        screenColor = (Button)findViewById(R.id.screenColor);
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativiveLayout);
+        screenColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                relativeLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+            }
+        });
     }
 }
